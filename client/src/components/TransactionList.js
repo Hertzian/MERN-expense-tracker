d5f3659a0f232d1ef60ffdca408d2441ef6ccc25
Lalitus <tallitosan@gmail.com>
2020-12-29 15:37:32 -0600

    added backend installed express, concurrently, mongoose, morga, etc

---
+++ client/src/components/TransactionList.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import Transaction from './Transaction'
+import { GlobalContext } from '../context/GlobalState'
+
+const TransactionList = () => {
+  // const context = useContext(GlobalContext)
+  const { transactions } = useContext(GlobalContext)
+
+  // console.log(context)
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className='list'>
+        {transactions.map((trans) => (
+          <Transaction key={trans.id} transaction={trans} />
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default TransactionList
